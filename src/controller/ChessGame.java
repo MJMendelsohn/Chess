a2d6651a4c38b4fc941b95a544f9e5ab64b149bf
MJMendelsohn <mjm111@duke.edu>
2015-03-16 16:13:20 -0400

    Created Main ChessGame

---
+++ src/controller/ChessGame.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChessGame extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+	
+	}
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
