63493c1b036db4196b7e623861bef3b609d699ba
MJMendelsohn <mjm111@duke.edu>
2015-03-16 16:15:07 -0400

    Created Turn

---
+++ src/model/Turn.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Turn {
+	
+	private Player myActive;
+	private int myCount;
+	
+	public boolean checkActive(Player player) {
+		return myActive.equals(player);
+	}
+	
+	public void setActive(Player player) {
+		myActive = player;
+	}
+	
+	public void updateTurn(Player player) {
+		setActive(player);
+		
+	}
+	
+	public void incrementCount() {
+		myCount++;
+	}
+	
+}
